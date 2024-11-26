2e198886277a5bd8ad9750171865a57780dbd52b
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2024-11-26 16:06:10 +0530

    fun

---
+++ Jdbc/src/CallableFunction.java
@@ -0,0 +1,23 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class CallableFunction {
+	public static void main(String[] args) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "dinesh", "dinesh9999");
+			CallableStatement cs=con.prepareCall("{call totalstudents()}");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.execute();
+			System.out.println(cs.getInt(1));
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+//error
